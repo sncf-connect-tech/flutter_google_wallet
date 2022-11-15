b4a41d2c9ebf38692f89118754f6df5109101ad8
adrien_body <abody@oui.sncf>
2022-11-15 10:15:53 +0100

    add language and button options

--- lib/widget/add_to_google_wallet_button.dart
+++ lib/widget/add_to_google_wallet_button.dart
@@ -5,14 +5,17 @@ import 'package:flutter_svg/flutter_svg.dart';
 class AddtoGoogleWalletButton extends StatelessWidget {
   // TODO gérer de passer la langue
   final VoidCallback? onPress;
-  const AddtoGoogleWalletButton({Key? key, this.onPress}) : super(key: key);
+  final bool badgeButton;
+  final String langue;
+  const AddtoGoogleWalletButton({Key? key, this.onPress, this.badgeButton = false, required this.langue}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final buttonPrefix = badgeButton ? 'add-wallet-badge' : 'wallet-button';
     // InkWell ?
     return Column(
       children: [
-        GestureDetector(onTap: onPress,child: SvgPicture.asset(package: 'flutter_google_wallet','assets/svg/button/frFR_add_to_google_wallet_wallet-button.svg')),
+        GestureDetector(onTap: onPress,child: SvgPicture.asset(package: 'flutter_google_wallet','assets/svg/button/${langue}_add_to_google_wallet_$buttonPrefix.svg')),
       ],
     );
   }
