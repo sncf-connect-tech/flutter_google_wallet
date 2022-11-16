253d91fba3f49a289ccffea7eaa5d49b72f95053
adrien_body <abody@oui.sncf>
2022-11-16 10:34:49 +0100

    add activity debug info on save wallet

--- android/src/main/kotlin/fr/sncf_connect/wallet/flutter_google_wallet/FlutterGoogleWalletPlugin.kt
+++ android/src/main/kotlin/fr/sncf_connect/wallet/flutter_google_wallet/FlutterGoogleWalletPlugin.kt
@@ -2,6 +2,7 @@ package fr.sncf_connect.wallet.flutter_google_wallet
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import androidx.annotation.NonNull
 import com.google.android.gms.pay.Pay
 import com.google.android.gms.pay.PayApiAvailabilityStatus
@@ -11,9 +12,10 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.PluginRegistry
 
 
-class FlutterGoogleWalletPlugin: FlutterPlugin, Messages.GoogleWalletApi, ActivityAware {
+class FlutterGoogleWalletPlugin: FlutterPlugin, Messages.GoogleWalletApi, ActivityAware, PluginRegistry.ActivityResultListener {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -55,4 +57,14 @@ class FlutterGoogleWalletPlugin: FlutterPlugin, Messages.GoogleWalletApi, Activi
   override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
 
   override fun onDetachedFromActivity() {}
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
+    if (resultCode == PayClient.SavePassesResult.SAVE_ERROR && data != null) {
+      val errorMessage = data.getStringExtra(PayClient.EXTRA_API_ERROR_MESSAGE)
+      print("ERROR ON GOOGLE WALLET SAVE : $errorMessage")
+    } else {
+      print("ADD TO GOOGLE WALLET SUCCEED : $resultCode $data")
+    }
+    return true
+  }
 }
