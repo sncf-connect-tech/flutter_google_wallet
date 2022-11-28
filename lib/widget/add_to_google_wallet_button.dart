52cad88f1bb7581a6159dd094fdb2e9357b3fb0a
adrien_body <abody@oui.sncf>
2022-11-28 12:59:57 +0100

    reformat

--- lib/widget/add_to_google_wallet_button.dart
+++ lib/widget/add_to_google_wallet_button.dart
@@ -7,17 +7,21 @@ class AddtoGoogleWalletButton extends StatelessWidget {
   final VoidCallback? onPress;
   final bool badgeButton;
   final String langue;
-  const AddtoGoogleWalletButton({Key? key, this.onPress, this.badgeButton = false, required this.langue}) : super(key: key);
+
+  const AddtoGoogleWalletButton(
+      {Key? key, this.onPress, this.badgeButton = false, required this.langue})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final buttonPrefix = badgeButton ? 'add-wallet-badge' : 'wallet-button';
-    final path = 'assets/svg/button/${langue}_add_to_google_wallet_$buttonPrefix.svg';
+    final path =
+        'assets/svg/button/${langue}_add_to_google_wallet_$buttonPrefix.svg';
     return Column(
       children: [
-        GestureDetector(onTap: onPress,
-            child: SvgPicture.asset(package: 'flutter_google_wallet', path)
-        ),
+        GestureDetector(
+            onTap: onPress,
+            child: SvgPicture.asset(package: 'flutter_google_wallet', path)),
       ],
     );
   }
