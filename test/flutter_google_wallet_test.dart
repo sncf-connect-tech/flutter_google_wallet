34bafd9bb40e18188ffd3480e009bacfbe0d8b1d
adrien_body <abody@oui.sncf>
2022-11-28 14:49:31 +0100

    add test folder

--- test/flutter_google_wallet_test.dart
+++ test/flutter_google_wallet_test.dart
@@ -1,1 +1,34 @@
-void main() {}
+import 'package:flutter_google_wallet/flutter_google_wallet_pigeon.dart';
+import 'package:flutter_google_wallet/flutter_google_wallet_platform_interface.dart';
+import 'package:flutter_google_wallet/flutter_google_wallet_plugin.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockFlutterGoogleWalletPlatform
+    with MockPlatformInterfaceMixin
+    implements FlutterGoogleWalletPlatform {
+  @override
+  Future<bool> getWalletApiAvailabilityStatus() => Future.value(true);
+
+  @override
+  Future<void> initWalletClient() => Future.value();
+
+  @override
+  Future<void> savePasses({required String jsonPass, required int addToGoogleWalletRequestCode}) => Future.value();
+}
+
+void main() {
+  final FlutterGoogleWalletPlatform initialPlatform = FlutterGoogleWalletPlatform.instance;
+
+  test('$FlutterGoogleWalletPlugin is the default instance', () {
+    expect(initialPlatform, isInstanceOf<PigeonFlutterGoogleWallet>());
+  });
+
+  test('getWalletApiAvailabilityStatus', () async {
+    FlutterGoogleWalletPlugin flutterGoogleWalletPlugin = FlutterGoogleWalletPlugin();
+    MockFlutterGoogleWalletPlatform fakePlatform = MockFlutterGoogleWalletPlatform();
+    FlutterGoogleWalletPlatform.instance = fakePlatform;
+
+    expect(await flutterGoogleWalletPlugin.getWalletApiAvailabilityStatus(), true);
+  });
+}
