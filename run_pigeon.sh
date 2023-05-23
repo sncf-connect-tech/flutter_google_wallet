f7a0baab6ccdfd215100e946d5140edf7873b292
Emmanuel Lefebvre <Emmanuel_LEFEBVRE@connect-tech.sncf>
2023-05-23 13:49:17 +0200

    build: support Flutter 3.10 / Dart 3.0.0

--- run_pigeon.sh
+++ run_pigeon.sh
@@ -1,4 +1,4 @@
-flutter pub run pigeon \
+dart pub run pigeon \
   --input pigeons/messages.dart \
   --dart_out lib/messages.dart \
   --java_out android/src/main/java/sncf/connect/tech/flutter_google_wallet/Messages.java \
